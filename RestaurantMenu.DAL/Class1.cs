7d6cee22601c4f00d1f740af4e03851c45873ecb
troybrox <danilalekseew@gmail.com>
2020-02-28 00:17:28 +0400

    added: data access layer -project, business logic layer -project

---
+++ RestaurantMenu.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestaurantMenu.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ RestaurantMenu.DLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RestaurantMenu.DLL
+{
+    public class Class1
+    {
+    }
+}
