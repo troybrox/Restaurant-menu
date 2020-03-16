3dbdade05edd2bc192eade92be5a2ad057177bf9
troybrox <danilalekseew@gmail.com>
2020-03-17 01:48:05 +0400

    little fix

--- RestaurantMenu/Startup.cs
+++ RestaurantMenu/Startup.cs
@@ -32,8 +32,8 @@ namespace RestaurantMenu
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<MenuDBContext>( optionsBuiller =>
-            optionsBuiller.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContextPool<MenuDBContext>( optionsBuilder =>
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddCors(options =>
             {
@@ -67,30 +67,15 @@ namespace RestaurantMenu
                 app.UseHsts();
             }
 
-            //app.UseCors(options =>
-            //    options.WithOrigins("https://localhost:44360", "http://localhost:44318", "https://localhost:44318")
-            //    .AllowAnyMethod()
-            //    .AllowAnyHeader()
-            //    .AllowCredentials()
-            //    );
-
-
-
             app.UseCors(MyAllowSpecificOrigins);
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapRazorPages();
                 endpoints.MapControllers();
             });   
         }
     }
-
-    public class CorsMidlware { }
 }
